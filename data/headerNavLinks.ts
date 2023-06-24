7d70ad463ac942ffcfb86a706f97efa1c3dc71ee
Benjamin Bialy <benjamincbialy@gmail.com>
2023-06-24 15:18:10 +1000

    removed blog

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/about', title: 'About' },
-  { href: '/blog', title: 'Blog' },
+  // { href: '/blog', title: 'Blog' },
   // { href: '/courses', title: 'Courses' },
   { href: '/projects', title: 'Projects' },
   { href: '/contact', title: 'Contact' },
